41c060efef468827ca16344f43eb86568c510ab9
CromonMS <e@cromon.co.uk>
2018-03-21 11:39:43 +0000

    Add copyright to Page Footer, and added simple page transition

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app" class="gradient">
-    <div class="content">
-      <!-- <nav-bar></nav-bar> -->
-      <app-hero></app-hero>
-      <router-view/>
-      <app-footer></app-footer>
+  <transition name="fade">  
+    <div id="app" class="gradient">
+      <div class="content"
+        <!-- <nav-bar></nav-bar> -->
+        <app-hero></app-hero>
+        <transition name="fade">
+          <router-view/>
+        </transition>
+        <app-footer></app-footer>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -24,4 +28,16 @@ export default {
 </script>
 
 <style>
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,14 @@
 <template>
-  <section class="section gradient">
+  <section class="section">
     <footer class="footer gradient">
-      FOOTER
+      <div class="columns">
+        <div class="column is-one-third">
+          <p>&copy; 2018 Cromon Media Services</p>
+          <p>Part of the <a href="http://www.cromon.co.uk" target="_blank">Cromon</a> Network</p>
+        </div>
+        <div class="column"></div>
+        <div class="column"></div>
+      </div>
     </footer>
   </section>
 </template>
