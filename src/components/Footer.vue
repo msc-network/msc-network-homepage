ede6f7ca1d2cb0b5f4a271a00f7bdfe783bb0fd9
cromon <e@cromon.co.uk>
2022-05-08 22:51:39 +0100

    homepage

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="columns">
       <div class="column is-one-third">
-        <p>&copy; 2018 Cromon Media Services</p>
+        <p>&copy; 2021 Cromon Ltd</p>
         <p>Part of the <a href="http://www.cromon.co.uk" target="_blank">Cromon</a> Network</p>
       </div>
       <div class="column"></div>
